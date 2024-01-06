2694e927858b5e6ac84d7d52974335de65d42947
Jan Spalding <147877949+Jan-Spalding@users.noreply.github.com>
2024-01-06 17:04:16 +0000

    Update OrbitControls.js

--- Import/Three.js/OrbitControls.js
+++ Import/Three.js/OrbitControls.js
@@ -6,7 +6,7 @@ import {
 	TOUCH,
 	Vector2,
 	Vector3
-} from './Import/Three.js/three.module.js';
+} from 'Doom-Nuclear-Fallout-Game/Import/Three.js/three.module.js';
 
 // This set of controls performs orbiting, dollying (zooming), and panning.
 // Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
